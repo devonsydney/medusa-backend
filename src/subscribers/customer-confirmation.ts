fb7a5fe3d88543251947318a40cdb2bff3be1205
devonsydney <devon.boultonmills@gmail.com>
2023-05-13 16:36:11 +0100

    extending customer confirmation event handler

--- src/subscribers/customer-confirmation.ts
+++ src/subscribers/customer-confirmation.ts
@@ -3,6 +3,8 @@ import { Customer, EventBusService } from "@medusajs/medusa"
 const SENDGRID_CUSTOMER_CONFIRMATION = process.env.SENDGRID_CUSTOMER_CONFIRMATION
 const SENDGRID_FROM = process.env.SENDGRID_FROM
 const STORE_URL = process.env.STORE_URL
+const STORE_NAME = process.env.STORE_NAME
+const STORE_LOGO = process.env.STORE_LOGO
 
 type InjectedDependencies = {
   eventBusService: EventBusService,
@@ -32,7 +34,9 @@ class CustomerConfirmationSubscriber {
         email: data.email,
         first_name: data.first_name,
         last_name: data.last_name,
-        store_url: STORE_URL
+        store_url: STORE_URL,
+        store_name: STORE_NAME,
+        store_logo: STORE_LOGO
         /*data*/ /* add in to see the full data object returned by the event */
       },
     })
