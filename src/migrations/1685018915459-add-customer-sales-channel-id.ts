86a41c5ae1661ee11c9881c0afba95ea9e8a4308
devonsydney <devon.boultonmills@gmail.com>
2023-05-25 16:09:16 +0100

    extending customer to have a column for sales column (model, repository & migration)

---
+++ src/migrations/1685018915459-add-customer-sales-channel-id.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddCustomerSalesChannelId1685018915459 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "customer" ADD "sales_channel_id" character varying`);
+        await queryRunner.query(`CREATE INDEX "CustomerSalesChannelId" ON "customer" ("sales_channel_id") `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."CustomerSalesChannelId"`);
+        await queryRunner.query(`ALTER TABLE "customer" DROP COLUMN "sales_channel_id"`);
+    }
+
+}
---
+++ src/repositories/customer.ts
@@ -0,0 +1,14 @@
+import { Customer } from "../models/customer"
+import { dataSource } from "@medusajs/medusa/dist/loaders/database"
+import { CustomerRepository as MedusaCustomerRepository } from "@medusajs/medusa/dist/repositories/customer"
+
+export const CustomerRepository = dataSource
+  .getRepository(Customer)
+  .extend({
+    ...Object.assign(
+      MedusaCustomerRepository, 
+      { target: Customer }
+    ),
+  })
+
+export default CustomerRepository
