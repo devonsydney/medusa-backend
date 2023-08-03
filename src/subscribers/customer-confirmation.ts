f0544f8a7832306ecd1f915a8b27633512ec28c3
devonsydney <devon.boultonmills@gmail.com>
2023-08-02 19:17:16 -0700

    separating functions in customer confirmation subscriber

--- src/subscribers/customer-confirmation.ts
+++ src/subscribers/customer-confirmation.ts
@@ -1,6 +1,6 @@
 import { Customer, EventBusService } from "@medusajs/medusa"
-import { debugLog } from "../scripts/debug"
 import { getProfileByEmail, createProfile } from "../scripts/klaviyo"
+import { debugLog } from "../scripts/debug"
 
 const SENDGRID_CUSTOMER_CONFIRMATION = process.env.SENDGRID_CUSTOMER_CONFIRMATION
 const SENDGRID_FROM = process.env.SENDGRID_FROM
@@ -31,48 +31,57 @@ class CustomerConfirmationSubscriber {
     debugLog("handleCustomerConfirmation running...")
     if (data.has_account) {
       debugLog("customer has account...")
-      // SENDGRID
-      debugLog("using template ID:", SENDGRID_CUSTOMER_CONFIRMATION)
-      debugLog("using STORE_URL value:", STORE_URL)
-      debugLog("sending email to:", data.email)
-      
-      this.sendGridService.sendEmail({
-        templateId: SENDGRID_CUSTOMER_CONFIRMATION,
-        from: SENDGRID_FROM,
-        to: data.email,
-        dynamic_template_data: {
-          email: data.email,
-          first_name: data.first_name,
-          last_name: data.last_name,
-          store_url: STORE_URL,
-          store_name: STORE_NAME,
-          store_logo: STORE_LOGO
-          /*data*/ /* add in to see the full data object returned by the event */
-        },
-      })
-      // KLAVIYO
-      // Check if profile exists
-      debugLog("Check if profile exists in Klaviyo...")
-      const profiles = await getProfileByEmail(data.email)
-      debugLog("Profiles returned (0 or 1):", profiles.data.length)
+      this.sendgridEmail(data)
+      this.klaviyoCreateProfile(data)
+    }
+  }
 
-      // If profile does not exist, create it
-      if (!profiles.data.length) {
-        debugLog("Klaviyo profile does not exist, creating...")
-        const newProfile = {
-          email: data.email,
-          properties: {
-            store_name: STORE_NAME
-          }
-          // first_name: data.first_name,
-          // last_name: data.last_name
-          // Add more attributes if needed
+  // SendGrid Email Handler
+  sendgridEmail = (data: any) => {
+    debugLog("sending email to:", data.email)
+    debugLog("using template ID:", SENDGRID_CUSTOMER_CONFIRMATION)
+    debugLog("using STORE_URL value:", STORE_URL)
+    
+    this.sendGridService.sendEmail({
+      templateId: SENDGRID_CUSTOMER_CONFIRMATION,
+      from: SENDGRID_FROM,
+      to: data.email,
+      dynamic_template_data: {
+        email: data.email,
+        first_name: data.first_name,
+        last_name: data.last_name,
+        store_url: STORE_URL,
+        store_name: STORE_NAME,
+        store_logo: STORE_LOGO
+        /*data*/ /* add in to see the full data object returned by the event */
+      },
+    })    
+  }
+
+  // Klaviyo Profile Handler
+  klaviyoCreateProfile = async (data: any) => {
+    // Check if profile exists
+    debugLog("Check if profile exists in Klaviyo...")
+    const profiles = await getProfileByEmail(data.email)
+    debugLog("Profiles returned (0 or 1):", profiles.data.length)
+
+    // If profile does not exist, create it
+    if (!profiles.data.length) {
+      debugLog("Klaviyo profile does not exist, creating...")
+      const newProfile = {
+        email: data.email,
+        first_name: data.first_name,
+        last_name: data.last_name,
+        phone_number: data.phone,
+        // Add more attributes if needed
+        properties: {
+          store_name: STORE_NAME
         }
-        const createdProfile = await createProfile(newProfile)
-        debugLog("Profile created:", createdProfile)
-      } else {
-        debugLog("Profile already exists.")
       }
+      const createdProfile = await createProfile(newProfile)
+      debugLog("Profile created:", createdProfile)
+    } else {
+      debugLog("Profile already exists.")
     }
   }
 }
