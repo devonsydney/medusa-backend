62b8b71a3d767530a12ff84b7c952919797431aa
devonsydney <devon.boultonmills@gmail.com>
2023-11-04 10:42:42 +0000

    migrating middlewares from individual files to new medusa method in one file

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -1,6 +1,18 @@
+import type { MiddlewaresConfig } from "@medusajs/medusa"
+import type { 
+  MedusaNextFunction, 
+  MedusaRequest, 
+  MedusaResponse,
+} from "@medusajs/medusa"
 import PublishableApiKeyService from "@medusajs/medusa/dist/services/publishable-api-key"
+import { debugLog } from "../scripts/debug"
 
-export async function registerSalesChannelID(req, res, next) {
+const salesChannelMiddleware = async (
+  req: MedusaRequest, 
+  res: MedusaResponse, 
+  next: MedusaNextFunction
+) => {
+  debugLog("running salesChannelMiddleware")
   // resolve publishable key service
   const publishableKeyService: PublishableApiKeyService = req.scope.resolve("publishableApiKeyService")
   // Retrieve x-publishable-api-key
@@ -10,12 +22,24 @@ export async function registerSalesChannelID(req, res, next) {
   // return key of first sales channel
   const sales_channel_id = String(channels[0]?.id)
 
+  debugLog("pubKey",pubKey)
+  debugLog("sales_channel_id",sales_channel_id)
+
   // Register the Sales Channel ID
   req.scope.register({
     salesChannelID: {
       resolve: () => sales_channel_id,
     },
   })
-  
+
   next()
+}
+
+export const config: MiddlewaresConfig = {
+  routes: [
+    {
+      matcher: "/store/*",
+      middlewares: [salesChannelMiddleware],
+    },
+  ],
 }
