ad946ef35add2224463b0dcae05c9a577f8f46ef
devonsydney <devon.boultonmills@gmail.com>
2024-03-31 23:27:33 +0100

    adding unique index to migration

--- src/migrations/1711895554785-remove-customer-uniqueness-index.ts
+++ src/migrations/1711895554785-remove-customer-uniqueness-index.ts
@@ -4,9 +4,11 @@ export class RemoveCustomerUniquenessIndex1711895554785 implements MigrationInte
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP INDEX "IDX_unique_email_for_guests_and_customer_accounts"`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_unique_email_sales_channel_for_guests_and_customer_accounts" ON "customer" ("email", "has_account", "sales_channel_id") WHERE "deleted_at" IS NULL;`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "IDX_unique_email_sales_channel_for_guests_and_customer_accounts"`);
         await queryRunner.query(`CREATE UNIQUE INDEX "IDX_unique_email_for_guests_and_customer_accounts" ON "customer" ("email", "has_account") WHERE "deleted_at" IS NULL`);
     }
 }
