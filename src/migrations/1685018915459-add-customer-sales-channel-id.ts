30c31fe6afbb481a1058e2b89f33b2dbbfadfa24
devonsydney <devon.boultonmills@gmail.com>
2024-03-31 15:06:52 +0100

    breaking change: modifying old migration to not drop constraint for UQ_unique_email_for_guests_and_customer_accounts because later medusa migration (in v.1.20.3) drops this constraint - requires manually adding back in that constraint if the migration was run already

--- src/migrations/1685018915459-add-customer-sales-channel-id.ts
+++ src/migrations/1685018915459-add-customer-sales-channel-id.ts
@@ -4,7 +4,6 @@ export class AddCustomerSalesChannelId1685018915459 implements MigrationInterfac
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "customer" ADD "sales_channel_id" character varying`);
-        await queryRunner.query(`ALTER TABLE "customer" DROP CONSTRAINT "UQ_unique_email_for_guests_and_customer_accounts"`);
         await queryRunner.query(`ALTER TABLE "customer" ADD CONSTRAINT "UQ_unique_email_sales_channel" UNIQUE (email, has_account, sales_channel_id)`);
         await queryRunner.query(`CREATE INDEX "CustomerSalesChannelId" ON "customer" ("sales_channel_id") `);
     }
@@ -12,7 +11,6 @@ export class AddCustomerSalesChannelId1685018915459 implements MigrationInterfac
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP INDEX "public"."CustomerSalesChannelId"`);
         await queryRunner.query(`ALTER TABLE "customer" DROP CONSTRAINT "UQ_unique_email_sales_channel"`);
-        await queryRunner.query(`ALTER TABLE "customer" ADD CONSTRAINT "UQ_unique_email_for_guests_and_customer_accounts" UNIQUE (email, has_account)`);
         await queryRunner.query(`ALTER TABLE "customer" DROP COLUMN "sales_channel_id"`);
     }
 }
